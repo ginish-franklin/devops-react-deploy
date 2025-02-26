b61c3c884987fa28432d2fce7738abec6f78d8a4
ginish-franklin <ginish.franklin@gmail.com>
2025-02-26 18:06:07 +0000

    build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-docker build -t frankgin/devops-react-app:latest .
-docker tag frankgin/devops-react-app:latest frankgin/devops-react-app:latest
-docker push frankgin/devops-react-app:latest
+docker-compose build
